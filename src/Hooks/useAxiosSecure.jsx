c15f01f3dcd57e2c2b5384489efc6445f324c593
saarif23 <arif23s.001@gmail.com>
2023-11-22 19:14:28 +0600

    added with live link

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 
 export const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://bistro-boss-server-rosy-two.vercel.app'
 })
 
 const useAxiosSecure = () => {
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -89,7 +89,7 @@ const Routes = createBrowserRouter([
             {
                 path: "updateItem/:id",
                 element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/menu/${params.id}`)
+                loader: ({ params }) => fetch(`https://bistro-boss-server-rosy-two.vercel.app/menu/${params.id}`)
             },
             {
                 path: "users",
