276a68ba609f0ee90390e509c8d99d10df18534d
saarif23 <arif23s.001@gmail.com>
2024-02-08 18:40:40 +0600

    update

--- src/Pages/Dashborad/Payment/Payment.jsx
+++ src/Pages/Dashborad/Payment/Payment.jsx
@@ -1,7 +1,8 @@
 import { loadStripe } from "@stripe/stripe-js";
 import Title from "../../../Components/Title";
-import { Elements } from "@stripe/react-stripe-js";
+
 import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY)
 const Payment = () => {
