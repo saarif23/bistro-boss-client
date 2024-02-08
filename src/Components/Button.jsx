5cbe54def473c7630a98d57333a92d56bd56f966
saarif23 <arif23s.001@gmail.com>
2024-02-08 20:15:55 +0600

    modify button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Button = ({ btnText }) => {
   return (
-    <div className="uppercase border-b-4 mx-auto text-center font-semibold hover:animate-pulse  w-60 border-black hover:bg-black hover:text-white  rounded-xl px-5 py-2 shadow-lg">
+    <div className="uppercase border-b-4 mx-auto text-center font-semibold hover:animate-pulse  w-96 border-black hover:bg-black hover:text-white  rounded-xl px-5 py-2 shadow-lg">
       {btnText}
     </div>
   );
